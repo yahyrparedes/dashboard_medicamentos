e50aa0839d75fdca44b973d9dca4e995bd79d652
Yahyr Enrique Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2024-02-14 12:16:51 -0500

    Gender

--- database/seeders/GenderSeeder.php
+++ database/seeders/GenderSeeder.php
@@ -15,15 +15,31 @@ class GenderSeeder extends Seeder
      */
     public function run()
     {
-        //  \App\Models\User::factory(10)->create();
+
+//        DB::table('genders')->insert([
+//            'id' => 1,
+//            'name' => 'Masculino',
+//            'is_active' => true,
+//        ]);
+//        DB::table('genders')->insert([
+//            'id' => 2,
+//            'name' => 'Femenino',
+//            'is_active' => true,
+//        ]);
+//        DB::table('genders')->insert([
+//            'id' => 3,
+//            'name' => 'Otro',
+//            'is_active' => true,
+//        ]);
+
         DB::table('genders')->insert([
             'id' => 1,
-            'name' => 'Masculino',
+            'name' => 'Mujer',
             'is_active' => true,
         ]);
         DB::table('genders')->insert([
             'id' => 2,
-            'name' => 'Femenino',
+            'name' => 'Hombre',
             'is_active' => true,
         ]);
         DB::table('genders')->insert([
